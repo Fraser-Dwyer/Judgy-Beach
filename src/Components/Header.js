361b840a8971c70ed0d2282ecd054b11da2f8f4c
Fraser Dwyer <fraserd1413@gmail.com>
2023-12-14 15:42:52 +0000

    Header styling added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,19 @@
+import "../Styles/Header.css";
+import hamburgerIcon from "../Images/hamburgerIconWhite.png";
+import profileIcon from "../Images/profileIcon.png";
+
 export default function Header() {
   return (
     <>
-      <p>Header here</p>
+      <div className="headerContainer">
+        <div>
+          <img src={hamburgerIcon} alt="Hamburger menu icon" />
+        </div>
+        <p>Judgy Beach</p>
+        <div>
+          <img src={profileIcon} alt="Profile picture icon" />
+        </div>
+      </div>
     </>
   );
 }
