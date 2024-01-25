666f6afcb3bcce6bef77eb4bbf607d94b2ca832c
Fraser Dwyer <fraserd1413@gmail.com>
2024-01-25 12:43:24 +0000

    login signup css done mobile and desktop

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,9 +4,14 @@ import { googleLogout, useGoogleLogin } from "@react-oauth/google";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import googleLogo from "../Images/google-logo.png";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import PlayOnce from "../Components/PlayOnce";
+
+const Gavel = require("../Images/gavel-animation-black.json");
 
 export default function Login() {
   const navigate = useNavigate();
+  const isSmallDevice = useMediaQuery("only screen and (max-width : 900px)");
   const [user, setUser] = useState(null);
   const [profile, setProfile] = useState(null);
 
@@ -37,16 +42,40 @@ export default function Login() {
 
   return (
     <>
-      <div className="w-screen flex justify-center mt-[10vw] sm:mt-[75px]">
-        <div className="w-4/5 text-center text-[5vw] sm:w-[800px] sm:text-[25px] sm:flex">
-          <div className="w-full flex justify-center sm:items-center sm:w-1/2 sm:mr-[30px] sm:h-[60vh]">
+      <div className="w-screen mt-[10vw] sm:mt-0 sm:flex sm:justify-center sm:items-center">
+        <div className="sm:max-w-[800px] sm:w-[60%] sm:items-center">
+          <div className="flex justify-center items-center">
             <img
               src={judgyBeachLogo}
               alt="Judgy Beach Logo"
-              className="w-3/5 mb-[5vw] sm:min-w-[400px] sm:min-h-[400px] sm:mb-0"
+              className="w-[40%] mb-[2vw] mr-[3vw] sm:mb-0 sm:w-[300px] sm:h-[300px]"
             ></img>
+            <div className="w-[40%]">
+              <div className="flex items-center">
+                <p className="flex mr-[2vw] text-xxxl select-none sm:text-[65px] sm:mr-[15px]">
+                  Judgy
+                </p>
+                {isSmallDevice && <PlayOnce size="12vw" icon={Gavel} />}
+                {!isSmallDevice && <PlayOnce size="100px" icon={Gavel} />}
+              </div>
+              <p className="flex flex-1 text-[12vw] mt-[-2vw] select-none sm:text-[100px] sm:mt-[-20px]">
+                Beach
+              </p>
+            </div>
           </div>
-          <div className="py-[3vw] sm:w-[400px] sm:p-[10px]  sm:items-center">
+          {!isSmallDevice && (
+            <div className="ml-[70px] mt-[20px]">
+              <ul className="text-xxlPC [&>*]:mb-[15px] font-bold">
+                <li>Always have an opinion and need to share it?</li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <div className="py-[8vw] flex justify-center sm:p-[10px] sm:items-center sm:w-[40%] sm:h-[100dvh]">
+          <div className="w-3/4 borders bg-white p-[4vw] sm:max-w-[400px] sm:p-[20px]">
+            <p className="text-xl font-bold text-left mb-[2vw] sm:text-xlPC sm:mb-[10px]">
+              Log In
+            </p>
             <form className="">
               <div className="flex justify-center sm:justify-start">
                 <input className="input" placeholder="Username"></input>
@@ -54,11 +83,14 @@ export default function Login() {
               <div className="flex justify-center sm:justify-start">
                 <input className="input" placeholder="Password"></input>
               </div>
+              <div className="flex justify-center sm:justify-start">
+                <input className="input" placeholder="Repeat Password"></input>
+              </div>
               <div className="flex justify-center">
                 <button className="btn prim w-full">Log In</button>
               </div>
-              <p className="text-[3.5vw] pt-[2vw] sm:text-[17px]">
-                Don't have an account?{" "}
+              <p className="text-md pt-[2vw] flex justify-center gap-0.5 sm:text-mdPC sm:pt-[10px]">
+                Don't have an account?
                 <span
                   className="underline font-bold sm:hover:cursor-pointer"
                   onClick={() => navigate("/signup")}
@@ -67,20 +99,20 @@ export default function Login() {
                 </span>
               </p>
             </form>
-            <p className="text-[4vw] font-bold py-[2vw] sm:text-[20px] sm:py-[20px]">
+            <p className="text-md font-bold py-[2vw] flex justify-center sm:text-mdPC sm:py-[10px]">
               - OR -
             </p>
-            <div className="w-full flex text-[18px] justify-center sm:w-[385px]">
+            <div className="w-full flex justify-center">
               <div
                 onClick={() => login()}
-                className="flex justify-center items-center px-[2vw] py-[1vw] w-full bg-white borders rounded-md sm:px-[16px] sm:py-[8px]"
+                className="flex justify-center items-center px-[2vw] py-[2vw] w-full bg-white borders rounded-md sm:px-[16px] sm:py-[7px] sm:hover:cursor-pointer sm:hover:bg-gray-200"
               >
                 <img
                   src={googleLogo}
                   alt="Google Logo"
-                  className="w-[6vw] h-[6vw] mr-[3vw] sm:w-[35px] sm:h-[35px] sm:mr-[30px]"
+                  className="w-[4vw] h-[4vw] mr-[2vw] sm:w-[20px] sm:h-[20px] sm:mr-[15px]"
                 />
-                <p className="text-[4vw] sm:text-[18px]">Sign in with Google</p>
+                <p className="text-md sm:text-mdPC">Sign in with Google</p>
               </div>
             </div>
           </div>
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -42,24 +42,38 @@ export default function Signup() {
 
   return (
     <>
-      <div className="w-screen mt-[10vw] sm:mt-0 sm:flex">
-        <div className="flex justify-center items-center sm:w-[80%]">
-          <img
-            src={judgyBeachLogo}
-            alt="Judgy Beach Logo"
-            className="w-[40%] mb-[2vw] mr-[3vw] sm:mb-0 sm:w-[200px] sm:h-[200px]"
-          ></img>
-          <div className="w-[40%]">
-            <div className="flex items-center">
-              <p className="flex mr-[2vw] text-xxxl">Judgy</p>
-              <PlayOnce size="10vw" icon={Gavel} />
+      <div className="w-screen mt-[10vw] sm:mt-0 sm:flex sm:justify-center sm:items-center">
+        <div className="sm:max-w-[800px] sm:w-[60%] sm:items-center">
+          <div className="flex justify-center items-center">
+            <img
+              src={judgyBeachLogo}
+              alt="Judgy Beach Logo"
+              className="w-[40%] mb-[2vw] mr-[3vw] sm:mb-0 sm:w-[300px] sm:h-[300px]"
+            ></img>
+            <div className="w-[40%]">
+              <div className="flex items-center">
+                <p className="flex mr-[2vw] text-xxxl select-none sm:text-[65px] sm:mr-[15px]">
+                  Judgy
+                </p>
+                {isSmallDevice && <PlayOnce size="12vw" icon={Gavel} />}
+                {!isSmallDevice && <PlayOnce size="100px" icon={Gavel} />}
+              </div>
+              <p className="flex flex-1 text-[12vw] mt-[-2vw] select-none sm:text-[100px] sm:mt-[-20px]">
+                Beach
+              </p>
             </div>
-            <p className="flex flex-1 text-[12vw] mt-[-2vw]">Beach</p>
           </div>
+          {!isSmallDevice && (
+            <div className="ml-[70px] mt-[20px]">
+              <ul className="text-xxlPC [&>*]:mb-[15px] font-bold">
+                <li>Always have an opinion and need to share it?</li>
+              </ul>
+            </div>
+          )}
         </div>
-        <div className="py-[3vw] flex justify-center sm:p-[10px] sm:items-center sm:w-[40%]">
-          <div className="w-3/4 sm:max-w-[400px]">
-            <p className="text-xl font-bold text-center mb-[2vw] sm:text-xlPC sm:mb-[10px]">
+        <div className="py-[8vw] flex justify-center sm:p-[10px] sm:items-center sm:w-[40%] sm:h-[100dvh]">
+          <div className="w-3/4 borders bg-white p-[4vw] sm:max-w-[400px] sm:p-[20px]">
+            <p className="text-xl font-bold text-left mb-[2vw] sm:text-xlPC sm:mb-[10px]">
               Sign Up
             </p>
             <form className="">
@@ -75,23 +89,23 @@ export default function Signup() {
               <div className="flex justify-center">
                 <button className="btn prim w-full">Sign Up</button>
               </div>
-              <p className="text-md pt-[2vw] flex justify-center sm:text-mdPC">
-                Already have an account?{" "}
+              <p className="text-md pt-[2vw] flex justify-center gap-0.5 sm:text-mdPC sm:pt-[10px]">
+                Already have an account?
                 <span
-                  className="underline font-bold ml-[1vw] sm:hover:cursor-pointer"
+                  className="underline font-bold sm:hover:cursor-pointer"
                   onClick={() => navigate("/login")}
                 >
-                  {""} Log in here
+                  Log in here
                 </span>
               </p>
             </form>
-            <p className="text-md font-bold py-[2vw] flex justify-center sm:text-mdPC sm:py-[20px]">
+            <p className="text-md font-bold py-[2vw] flex justify-center sm:text-mdPC sm:py-[10px]">
               - OR -
             </p>
             <div className="w-full flex justify-center">
               <div
                 onClick={() => login()}
-                className="flex justify-center items-center px-[2vw] py-[1vw] w-full bg-white borders rounded-md sm:px-[16px] sm:py-[7px]"
+                className="flex justify-center items-center px-[2vw] py-[2vw] w-full bg-white borders rounded-md sm:px-[16px] sm:py-[7px] sm:hover:cursor-pointer sm:hover:bg-gray-200"
               >
                 <img
                   src={googleLogo}
